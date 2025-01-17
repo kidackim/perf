6ac0d7fcae53d8ccd2fb4bee5818b6f55c25bfb2
kidac <kidackim@gmail.com>
2025-01-17 14:04:28 +0100

    test

--- typescript/src/steps/search.ts
+++ typescript/src/steps/search.ts
@@ -1,10 +1,13 @@
-export const verifyTypedJsonResponse = exec(
+import { exec } from "@gatling.io/core";
+import { http, jsonPath } from "@gatling.io/http";
+
+export const verifyJsonResponse = exec(
   http("Verify JSON Response")
-    .get("/computers") // Twój endpoint
+    .get("/computers") // Twój endpoint API
     .check(
-      jsonPath("$.property1").is((value) => typeof value === "string"), // Sprawdź, czy property1 to string
-      jsonPath("$.property2").is((value) => typeof value === "string"), // Sprawdź, czy property2 to string
-      jsonPath("$.property3").is((value) => typeof value === "number"), // Sprawdź, czy property3 to liczba
-      jsonPath("$.property4").is((value) => typeof value === "number")  // Sprawdź, czy property4 to liczba
+      jsonPath("$.property1").is("value1"), // Sprawdź, czy `property1` ma wartość "value1"
+      jsonPath("$.property2").exists(),     // Sprawdź, czy `property2` istnieje
+      jsonPath("$.property3").ofType("NUMBER"), // Sprawdź, czy `property3` to liczba
+      jsonPath("$.property4").is("456")     // Sprawdź, czy `property4` ma wartość "456"
     )
 );
