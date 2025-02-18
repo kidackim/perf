787e7894559007dc086f25291afb10af5af1d6d6
kidackim <kidackim@gmail.com>
2025-02-18 13:24:17 +0100

    Update plik.sh

--- plik.sh
+++ plik.sh
@@ -1,6 +1,9 @@
-- Added a test to verify the POST /tokens/info endpoint.
-- Introduced an authentication mechanism that fetches an access token from Keycloak before test execution.
-- The access token is stored in the .env file, ensuring reusability across multiple test scenarios.
-- Updated Bash command to read all .env variables and set them as system environment variables.
-- Ensured Gatling has access to authentication credentials without modifying individual test scenarios.
-- Decoupled authentication logic from the test scenario for better maintainability and reusability.
+Refactor and extend LCM flow tests in Gatling
+
+- Refactored baseUrl handling for improved flexibility and maintainability.
+- Restructured service directories in test files for better organization.
+- Added additional steps to existing scenarios for more comprehensive testing.
+- Migrated LCM flow test (block, unblock, remove tokens) from Cypress to Gatling.
+- Updated and improved documentation for clarity and accuracy.
+
+This commit enhances test coverage, improves code structure, and aligns documentation with the latest changes.
